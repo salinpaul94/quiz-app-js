96b3e167373447e76d212293b0958855bff01370
salinpaul valooran <salinpv@hotmail.com>
2023-10-28 00:09:37 +0530

    function for start quiz

--- version_01/index.js
+++ version_01/index.js
@@ -43,3 +43,14 @@ const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next"
+  showQuestion();
+}
+
+function showQuestion() {
+  
+}
